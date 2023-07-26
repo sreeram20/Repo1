d345d0483391cd0551ffdd1df96c828dba3801dc
Sreeramr20 <80518862+Sreeramr20@users.noreply.github.com>
2023-07-26 18:47:24 +0530

    Add existing project
    :wq
    git status

---
+++ Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Sreeram R on 26/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
